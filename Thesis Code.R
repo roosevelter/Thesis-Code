59ef71eec9a9e4373ae02cd4a11e2c3b6f422573
roosevelter <lc1976@nyu.edu>
2014-11-11 02:06:16 -0500

    Update Thesis Code.R

--- Thesis Code.R
+++ Thesis Code.R
@@ -2,12 +2,10 @@
 
 no.doubt.terrorism = subset(terrorism, region == 10 & doubtterr == 0)
 
-foo = c(rep(NA, length(unique(no.doubt.terrorism$iyear))))
-fatality.years = c(rep(NA, length(unique(no.doubt.terrorism$iyear))))
+fatality.years = rep(NA, length(unique(no.doubt.terrorism$iyear)))
 
-for (i in 1:length(fatality.years) {
-  foo[i] = subset(no.doubt.terrorism, iyear == unique(no.doubt.terrorism$iyear)[i], select = nkill)
-  fatality.years[i] = colSums(foo[i], na.rm = T) 
+for (i in 1:length(fatality.years)) {
+  fatality.years[i] = colSums(subset(no.doubt.terrorism, iyear == unique(no.doubt.terrorism$iyear)[i], select = nkill))
 }
 
 no.doubt.terrorism = cbind(no.doubt.terrorism, fatality.years)
