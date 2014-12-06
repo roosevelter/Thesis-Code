5c4d2c8482e79fb880e7dfd8ee3c41b0aa147954
roosevelter <lc1976@nyu.edu>
2014-12-06 06:17:45 -0500

    Update Thesis Code.R

--- Thesis Code.R
+++ Thesis Code.R
@@ -4,3 +4,21 @@ no.doubt.terrorism = subset(terrorism, doubtterr == 0 & is.na(nkill) == F, selec
 no.doubt.terrorism = aggregate(x = setup, by = list(setup$country_txt, setup$iyear), FUN = function(x) sum(as.numeric(x)))
 
 
+Try to get the number of terrorist attacks for a given country-year.
+
+terrorist.attack.number = rep(NA, 43)
+
+
+for (j in unique(year)) {
+      for (i in 1:43) {
+          terrorist.attack.number[i] = nrow(no.doubt.terrorism[no.doubt.terrorism$iyear == j & no.doubt.terrorism$nkill != 0,])
+    }
+}
+
+
+  for (i in unique(year)) {
+    terrorist.attack.number[seq_along(i)] = nrow(no.doubt.terrorism[no.doubt.terrorism$iyear == i & no.doubt.terrorism$nkill != 0,])
+  }
+
+
+tapply(k, l, function (x) nrow)
