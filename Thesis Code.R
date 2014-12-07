e273a8ff3595d29596fcb112ebfd3b874b39a5db
roosevelter <lc1976@nyu.edu>
2014-12-07 05:26:36 -0500

    Update Thesis Code.R

--- Thesis Code.R
+++ Thesis Code.R
@@ -1,7 +1,10 @@
+# Set your goddamn working directory.
+setwd("C:/Users/lc1976/Desktop")
+
 # Summing up fatalities by country-year. Be sure to read in GTD data first.
 
 no.doubt.terrorism = subset(terrorism, doubtterr == 0 & is.na(nkill) == F, select = c(country_txt, nkill, iyear, country))
-no.doubt.terrorism = aggregate(x = no.doubt.terrorism, by = list(no.doubt.terrorism$country_txt, no.doubt.terrorism$iyear), FUN = function(x) sum(as.numeric(x)))
+possible.solution = aggregate(x = no.doubt.terrorism, by = list(no.doubt.terrorism$country_txt, no.doubt.terrorism$iyear), FUN = function(x) sum(as.numeric(x)))
 
 attack = vector("numeric", 3075)
 possible.solution = cbind(possible.solution, attack)
@@ -17,10 +20,13 @@ for (i in 1:length(possible.solution[,7])) {
 
 
 possible.solution = possible.solution[,-c(3, 5)]
-colnames(possible.solution) = c("Country", "Year", "Victims", "Country Code", "Terrorist Attack")
+colnames(possible.solution) = c("country", "year", "victims", "country code", "terrorist attack")
 
 # Cleaning the SWIID data.
+load("SWIIDv5_0.RData")
 
 swiid_data = lapply(swiid, function(x) x[x$year>=1970,, drop=F])
 swiid_data = as.data.frame(swiid_data)
 swiid_data = subset(swiid_data, is.na(gini_net) == F, select = c(country, year, gini_net))
+
+testMerge = merge(possible.solution, swiid_data, by = c("country", "year"))
