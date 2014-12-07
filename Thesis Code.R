91dab7f5bbb20b31c862d10012e33ff8cf126e75
roosevelter <lc1976@nyu.edu>
2014-12-07 00:51:04 -0500

    Update Thesis Code.R

--- Thesis Code.R
+++ Thesis Code.R
@@ -1,26 +1,18 @@
 # Summing up fatalities by country-year. Be sure to read in GTD data first.
 
 no.doubt.terrorism = subset(terrorism, doubtterr == 0 & is.na(nkill) == F, select = c(country_txt, nkill, iyear, country))
-no.doubt.terrorism = aggregate(x = setup, by = list(setup$country_txt, setup$iyear), FUN = function(x) sum(as.numeric(x)))
+no.doubt.terrorism = aggregate(x = no.doubt.terrorism, by = list(no.doubt.terrorism$country_txt, no.doubt.terrorism$iyear), FUN = function(x) sum(as.numeric(x)))
 
-possible.solution = aggregate(x = setup, by = list(countries, setup$iyear), FUN = function(x) sum(as.numeric(x)))
+attack = vector("numeric", 3075)
+possible.solution = cbind(possible.solution, attack)
 
-
-Try to get the number of terrorist attacks for a given country-year.
-
-terrorist.attack.number = rep(NA, 43)
-
-
-for (j in unique(year)) {
-      for (i in 1:43) {
-          terrorist.attack.number[i] = nrow(no.doubt.terrorism[no.doubt.terrorism$iyear == j & no.doubt.terrorism$nkill != 0,])
-    }
+for (i in 1:length(possible.solution[,7])) {
+  if (possible.solution$nkill[i] == 0) {
+    possible.solution[i,7] = 0
+  }
+  if (possible.solution$nkill[i] > 0) {
+    possible.solution[i,7] = 1
+  }
 }
 
 
-  for (i in unique(year)) {
-    terrorist.attack.number[seq_along(i)] = nrow(no.doubt.terrorism[no.doubt.terrorism$iyear == i & no.doubt.terrorism$nkill != 0,])
-  }
-
-
-tapply(k, l, function (x) nrow)
