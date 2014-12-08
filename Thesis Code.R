188bc2e365ba61e8f7882f66d72dba9cb12c45ca
roosevelter <lc1976@nyu.edu>
2014-12-08 06:51:49 -0500

    Update Thesis Code.R

--- Thesis Code.R
+++ Thesis Code.R
@@ -1,5 +1,9 @@
-# Set your goddamn working directory.
+# Before all of this, clean the data sets so that each data set has matching country names. They don't all include country codes.
+
+# Set your goddamn working directory. Load the required packages.
 setwd("C:/Users/lc1976/Desktop")
+install.packages(c("xlsx", "reshape2"))
+library(xlsx); library(reshape2)
 
 # Summing up fatalities by country-year. Be sure to read in GTD data first.
 
@@ -29,4 +33,33 @@ swiid_data = lapply(swiid, function(x) x[x$year>=1970,, drop=F])
 swiid_data = as.data.frame(swiid_data)
 swiid_data = subset(swiid_data, is.na(gini_net) == F, select = c(country, year, gini_net))
 
-testMerge = merge(possible.solution, swiid_data, by = c("country", "year"))
+first.merge = merge(possible.solution, swiid_data, by = c("country", "year"))
+
+# Load in the PRS data. 
+ethnic.tensions = read.xlsx("CountryData.xlsx", 1)
+ethnic.tensions = ethnic.tensions[ ,-2]
+ethnic.tensions = na.omit(ethnic.tensions)
+
+byapply <- function(x, by, fun, ...)
+{
+    # Create index list
+    if (length(by) == 1)
+    {
+        nc <- ncol(x)
+        split.index <- rep(1:ceiling(nc / by), each = by, length.out = nc)
+    } else # 'by' is a vector of groups
+    {
+        nc <- length(by)
+        split.index <- by
+    }
+    index.list <- split(seq(from = 1, to = nc), split.index)
+
+    # Pass index list to fun using sapply() and return object
+    sapply(index.list, function(i)
+            {
+                do.call(fun, list(x[, i], ...))
+            })
+}
+
+ethnic.tensions = byapply(ethnic.tensions[,3:ncol(ethnic.tensions) 12, rowMeans)
+colnames(ethnic.tensions) = c(country, c(1984:2013))
